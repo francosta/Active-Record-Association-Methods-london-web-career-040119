c251a68a9eaad74ed2905f298514c1ad7eb7b60a
francosta <francisco@fcosta.pt>
2019-04-14 20:42:53 +0100

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,18 +3,14 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    Song.all.select {|song| song.genre_id == self.id}.length
-  end
-
-  def artists
-    Song.all.map {|song| if song.genre_id == self.id then song.artist end}.uniq.compact
+    self.songs.length
   end
 
   def artist_count
-    artists.length
+    self.artists.length
   end
 
   def all_artist_names
-    artists.map {|artist| artist.name}
+    self.artists.map {|artist| artist.name}
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,11 +3,11 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
-    Genre.all.map {|genre| if genre.id == self.genre_id then genre.name end}
+    self.genre.name
   end
 
   def drake_made_this
-    self.artist_id = Artist.all.map {|artist| if artist.name == "Drake" then artist.id end}
+    self.artist = Artist.create(name: "Drake")
     # when this method is called it should assign the song's artist to Drake
   end
 end
